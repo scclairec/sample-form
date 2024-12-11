750f36be24bec0abb9597bf41d8b82293153f17f
Glitch (glitch-hello-node) <none>
2024-12-11 22:29:47 +0000

    🎖🍪 Checkpoint ./package.json:69970003/86 ./public/aboutus.html:69970003/370 ./public/index.html:69970003/521 ./server.js:69970003/1

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 // body-parser is used to read data payload from the http request body
 const bodyParser = require('body-parser'); 
 //  path is used to set default directories for MVC and also for the static files
-const path  = require('path'); 
+const path = require('path'); 
 // include the defined package
 
 
